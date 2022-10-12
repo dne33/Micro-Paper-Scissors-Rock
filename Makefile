fcccf0497cf9014acac9e3251c7d218d7b5f0320
Daniel Neal <youremail@uclive.ac.nz>
2022-10-12 22:21:43 +1300

    added send on button press

--- Makefile
+++ Makefile
@@ -16,7 +16,7 @@ all: game.out
 
 
 # Compile: create object files from C source files.
-game.o: game.c ../../drivers/avr/ir_uart.h ../../drivers/avr/system.h ../../drivers/display.h ../../drivers/navswitch.h ../../fonts/font5x7_1.h ../../utils/font.h ../../utils/pacer.h ../../utils/tinygl.h
+game.o: game.c ../../drivers/avr/ir_uart.h ../../drivers/avr/system.h ../../drivers/button.h ../../drivers/led.h ../../drivers/display.h ../../drivers/navswitch.h ../../fonts/font5x7_1.h ../../utils/font.h ../../utils/pacer.h ../../utils/tinygl.h
 	$(CC) -c $(CFLAGS) $< -o $@
 
 ir_uart.o: ../../drivers/avr/ir_uart.c ../../drivers/avr/ir_uart.h ../../drivers/avr/pio.h ../../drivers/avr/system.h ../../drivers/avr/timer0.h ../../drivers/avr/usart1.h
@@ -29,6 +29,12 @@ prescale.o: ../../drivers/avr/prescale.c ../../drivers/avr/prescale.h ../../driv
 	$(CC) -c $(CFLAGS) $< -o $@
 
 system.o: ../../drivers/avr/system.c ../../drivers/avr/system.h
+	$(CC) -c $(CFLAGS) $< -o $@
+
+button.o: ../../drivers/button.c ../../drivers/avr/pio.h ../../drivers/avr/system.h ../../drivers/button.h
+	$(CC) -c $(CFLAGS) $< -o $@
+
+led.o: ../../drivers/led.c ../../drivers/avr/pio.h ../../drivers/avr/system.h ../../drivers/led.h
 	$(CC) -c $(CFLAGS) $< -o $@
 
 timer.o: ../../drivers/avr/timer.c ../../drivers/avr/system.h ../../drivers/avr/timer.h
@@ -60,7 +66,7 @@ tinygl.o: ../../utils/tinygl.c ../../drivers/avr/system.h ../../drivers/display.
 
 
 # Link: create ELF output file from object files.
-game.out: game.o ir_uart.o pio.o prescale.o system.o timer.o timer0.o usart1.o display.o ledmat.o navswitch.o font.o pacer.o tinygl.o
+game.out: game.o ir_uart.o pio.o prescale.o system.o timer.o timer0.o usart1.o display.o ledmat.o navswitch.o font.o pacer.o tinygl.o button.o led.o
 	$(CC) $(CFLAGS) $^ -o $@ -lm
 	$(SIZE) $@
 
--- game.c
+++ game.c
@@ -3,6 +3,8 @@
 #include "navswitch.h"
 #include "ir_uart.h"
 #include "tinygl.h"
+#include "button.h"
+#include "led.h"
 #include "../fonts/font5x7_1.h"
 
 
@@ -32,6 +34,46 @@ void display_msg(char* message) {
         }
     }
 }
+void get_result(char player, char opponent) {
+    char result;
+    if (opponent == player) {
+        result = 'D';
+    }
+    else if (player == 'S' && opponent == 'R')
+    {
+        result = 'L';
+    }
+    else if (player == 'S' && opponent == 'P')
+    {
+        result = 'W';
+    }
+    else if (player == 'R' && opponent == 'P')
+    {
+        result = 'L';
+    }
+    else if (player == 'R' && opponent == 'S')
+    {
+        result = 'W';
+    }
+    else if (player == 'P' && opponent == 'R')
+    {
+        result = 'W';
+    }
+    else if (player == 'P' && opponent == 'S')
+    {
+        result = 'L';
+    }
+    if (result != '0')
+    {
+        if (result == 'L') {
+            display_msg("loser");
+        } else if (result == 'W') {
+            display_msg("winner");
+        } else {
+            display_msg("draw");
+        }
+    }
+}
 
 int main (void)
 {
@@ -39,11 +81,13 @@ int main (void)
     int index = 0;
     char rps[3] = {'R', 'P', 'S'};
     char player = '0';
+    char opponent = '0';
     system_init ();
     tinygl_init (PACER_RATE);
     tinygl_font_set (&font5x7_1);
     tinygl_text_speed_set (MESSAGE_RATE);
     navswitch_init ();
+    button_init ();
     ir_uart_init();
     pacer_init (PACER_RATE);
     
@@ -77,53 +121,27 @@ int main (void)
         }
 
         if (navswitch_push_event_p (NAVSWITCH_PUSH)) {
-            ir_uart_putc(rps[index]);
+            player = rps[index];
         }
+        
 
         if (ir_uart_read_ready_p ()) {
             char opponent;
             char result;
-            opponent = ir_uart_getc ();
-            if (opponent == 'R' || opponent == 'P' || opponent == 'S' ) {
-                if (opponent == player) {
-                result = 'D';
-                }
-                else if (player == 'S' && opponent == 'R')
-                {
-                    result = 'L';
-                }
-                else if (player == 'S' && opponent == 'P')
-                {
-                    result = 'W';
-                }
-                else if (player == 'R' && opponent == 'P')
-                {
-                    result = 'L';
-                }
-                else if (player == 'R' && opponent == 'S')
-                {
-                    result = 'W';
-                }
-                else if (player == 'P' && opponent == 'R')
-                {
-                    result = 'W';
-                }
-                else if (player == 'P' && opponent == 'S')
-                {
-                    result = 'L';
-                }
-                if (result != '0')
-                {
-                    if (result == 'L') {
-                        display_msg('loser');
-                    } else if (result == 'W') {
-                        display_msg('winner');
-                    } else {
-                        display_msg('draw');
-                    }
-                }
+            char ch;
+            ch = ir_uart_getc ();
+            if (ch == 'R' || ch == 'P' || ch == 'S' ) {
+                opponent = ch;
             } 
         } 
+        button_update();
+        if (button_push_event_p(0)) {
+            ir_uart_putc(rps[index]);
+        }
+        if (player != '0' && opponent != '0') {
+            ir_uart_putc(rps[index]);
+            get_result(player, opponent);
+        }
         display_character (rps[index]); 
     }
     return 0;
