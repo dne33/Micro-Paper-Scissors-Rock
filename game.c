bb1c4341c40813a15e5630535b3fdea5f39a1359
Daniel Neal <youremail@uclive.ac.nz>
2022-10-12 21:21:48 +1300

    made rps rotateable and added scrolling text

--- game.c
+++ game.c
@@ -18,17 +18,30 @@ void display_character (char character)
     tinygl_text (buffer);
 }
 
+void display_msg(char* message) {
+    bool displaying = true;
+    tinygl_text_mode_set (TINYGL_TEXT_MODE_SCROLL);
+    tinygl_text(message);
+    while (displaying) {
+        pacer_wait ();
+        tinygl_update ();
+        navswitch_update ();
+        if (navswitch_push_event_p (NAVSWITCH_NORTH)) {
+           displaying = false;
+        }
+    }
+}
 
 int main (void)
 {
-    char character = 'A';
-
+    int index = 0;
+    char rps[3] = {'R', 'P', 'S'};
     system_init ();
     tinygl_init (PACER_RATE);
     tinygl_font_set (&font5x7_1);
     tinygl_text_speed_set (MESSAGE_RATE);
     tinygl_text_mode_set (TINYGL_TEXT_MODE_SCROLL);
-    tinygl_text("SELECT: Rock Paper Scissors");
+    
     navswitch_init ();
     ir_uart_init();
     pacer_init (PACER_RATE);
@@ -36,36 +49,31 @@ int main (void)
 
     while (1)
     {
-        if (navswitch_push_event_p (NAVSWITCH_NORTH)) {
-            pacer_wait ();
-            tinygl_update ();
-        }
-        pacer_wait ();
-        tinygl_update ();
-        navswitch_update ();
 
-        
-        if (navswitch_push_event_p (NAVSWITCH_EAST))
-            character = 'R';
-            display_character (character);
-
-        if (navswitch_push_event_p (NAVSWITCH_SOUTH))
-            character = 'P';
-            display_character (character);
-
+        display_msg("SELECT: Rock Paper Scissors");
         if (navswitch_push_event_p (NAVSWITCH_WEST))
-            character = 'S';
-            display_character (character);
+            if (index == 0) {
+                index = 2;
+            } else {
+                index--;
+            }
 
         if (navswitch_push_event_p (NAVSWITCH_PUSH))
-            ir_uart_putc(character);
+            ir_uart_putc(rps[index]);
 
         if (ir_uart_read_ready_p ()) {
             char ch;
-            ch = ir_uart_getc ();
-            character = ch;
+            if ((ch = ir_uart_getc ()) == 'R' || ch=='P'||ch=='S') {
+                if (ch == 'R') {
+                    index = 0;
+                } else if (ch == 'P'){
+                    index = 1;
+                } else {
+                    index = 2;
+                }
+            } 
         }
-
+        display_character (rps[index]);
         
         }
 
