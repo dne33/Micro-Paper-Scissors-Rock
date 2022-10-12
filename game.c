e55e95e0a8f6bfaa6f0c0ffc7c5a9499779ce4f6
Matt Lane <mla159@cs19112bs.canterbury.ac.nz>
2022-10-12 21:20:23 +1300

    added win/loss logic

--- game.c
+++ game.c
@@ -21,14 +21,14 @@ void display_character (char character)
 
 int main (void)
 {
-    char character = 'A';
+    char player = '0';
 
     system_init ();
     tinygl_init (PACER_RATE);
     tinygl_font_set (&font5x7_1);
     tinygl_text_speed_set (MESSAGE_RATE);
     tinygl_text_mode_set (TINYGL_TEXT_MODE_SCROLL);
-    tinygl_text("SELECT: Rock Paper Scissors");
+    tinygl_text("Select: Rock Paper Scissors");
     navswitch_init ();
     ir_uart_init();
     pacer_init (PACER_RATE);
@@ -36,37 +36,65 @@ int main (void)
 
     while (1)
     {
-        if (navswitch_push_event_p (NAVSWITCH_NORTH)) {
-            pacer_wait ();
-            tinygl_update ();
-        }
         pacer_wait ();
         tinygl_update ();
         navswitch_update ();
 
         
         if (navswitch_push_event_p (NAVSWITCH_EAST))
-            character = 'R';
-            display_character (character);
+            player = 'R';
 
-        if (navswitch_push_event_p (NAVSWITCH_SOUTH))
-            character = 'P';
-            display_character (character);
+        if (navswitch_push_event_p (NAVSWITCH_SOUTH)) 
+            player = 'P';
 
-        if (navswitch_push_event_p (NAVSWITCH_WEST))
-            character = 'S';
-            display_character (character);
+        if (navswitch_push_event_p (NAVSWITCH_WEST)) 
+            player = 'S';
 
         if (navswitch_push_event_p (NAVSWITCH_PUSH))
-            ir_uart_putc(character);
-
-        if (ir_uart_read_ready_p ()) {
-            char ch;
-            ch = ir_uart_getc ();
-            character = ch;
-        }
+            ir_uart_putc(player);
 
         
-        }
-
+        if (ir_uart_read_ready_p ()) {
+            char opponent;
+            char result;
+            opponent = ir_uart_getc ();
+            if (opponent == player) {
+                result = 'D';
+            }
+            else if (player == 'S' && opponent == 'R')
+            {
+                result = 'L';
+            }
+            else if (player == 'S' && opponent == 'P')
+            {
+                result = 'W';
+            }
+            else if (player == 'R' && opponent == 'P')
+            {
+                result = 'L';
+            }
+            else if (player == 'R' && opponent == 'S')
+            {
+                result = 'W';
+            }
+            else if (player == 'P' && opponent == 'R')
+            {
+                result = 'W';
+            }
+            else if (player == 'P' && opponent == 'S')
+            {
+                result = 'L';
+            }
+            if (result != '0')
+            {
+                display_character(result);
+                pacer_wait();
+                pacer_wait();
+                pacer_wait();
+                pacer_wait();
+            }
+        }      
+    }
+    display_character (player);
 }
+
